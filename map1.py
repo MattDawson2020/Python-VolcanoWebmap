efaeae364a2ce885950d9155632cdf8c6c928ec4
Matt Dawson <mattdawson18@gmail.com>
2021-07-20 18:33:41 +0100

    Added selective polygon highlighting based on country population

--- map1.py
+++ map1.py
@@ -28,6 +28,11 @@ for lt, ln, ev, nm in zip(lat, lon, elev, name):
     iframe = folium.IFrame(html=html % (nm, nm, ev), width=200, height=100)
     fg.add_child(folium.Marker(location=[lt, ln], popup=folium.Popup(iframe), icon=folium.Icon(color=color_producer(ev))))
 
+
+fg.add_child(folium.GeoJson(data=open("world.json", "r", encoding="utf-8-sig").read(),
+style_function=lambda x: {'fillColor': 'green' if x["properties"]['POP2005'] < 10000000 
+else 'orange' if 10000000 <= x['properties']['POP2005'] < 50000000 else 'red'}))
+
 map.add_child(fg)
 
 map.save("Map1.html")
