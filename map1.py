30f58e6d77472ef1b18c77e2280c95a264f1cc77
Matt Dawson <mattdawson18@gmail.com>
2021-07-20 16:59:33 +0100

    Adding multiple markers using a loop

--- map1.py
+++ map1.py
@@ -2,7 +2,9 @@ import folium
 map = folium.Map(location=[38.58, -99.09], zoom_start=6, tiles="Stamen Terrain")
 
 fg = folium.FeatureGroup(name="My Map")
-fg.add_child(folium.Marker(location=[38.2, -99.1], popup="Hi I am a marker", icon=folium.Icon(color="lightblue")))
+
+for coordinates in [[38.2, -99.1], [39.2, -97.1]]:
+    fg.add_child(folium.Marker(location=coordinates, popup="Hi I am a marker", icon=folium.Icon(color="lightblue")))
 
 map.add_child(fg)
 
