2787bc1f75b6c88b1923ab6a63b568f0f69a7775
Matt Dawson <mattdawson18@gmail.com>
2021-07-20 18:40:35 +0100

    Used Layercontrol class to add the ability to turn the map layers on and off

--- map1.py
+++ map1.py
@@ -22,17 +22,21 @@ def color_producer(elevation):
         return "red"
 
 map = folium.Map(location=[38.58, -99.09], zoom_start=6, tiles="Stamen Terrain")
-fg = folium.FeatureGroup(name="My Map")
+fgv = folium.FeatureGroup(name="Volcano map")
 
 for lt, ln, ev, nm in zip(lat, lon, elev, name):
     iframe = folium.IFrame(html=html % (nm, nm, ev), width=200, height=100)
-    fg.add_child(folium.Marker(location=[lt, ln], popup=folium.Popup(iframe), icon=folium.Icon(color=color_producer(ev))))
+    fgv.add_child(folium.Marker(location=[lt, ln], popup=folium.Popup(iframe), icon=folium.Icon(color=color_producer(ev))))
 
 
-fg.add_child(folium.GeoJson(data=open("world.json", "r", encoding="utf-8-sig").read(),
+fgp = folium.FeatureGroup(name="Population map")
+fgp.add_child(folium.GeoJson(data=open("world.json", "r", encoding="utf-8-sig").read(),
 style_function=lambda x: {'fillColor': 'green' if x["properties"]['POP2005'] < 10000000 
 else 'orange' if 10000000 <= x['properties']['POP2005'] < 50000000 else 'red'}))
 
-map.add_child(fg)
+
+map.add_child(fgv)
+map.add_child(fgp)
+map.add_child(folium.LayerControl())
 
 map.save("Map1.html")
